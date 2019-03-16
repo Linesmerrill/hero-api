6c8d2a630310be438bea30ce1b26d4df1a27810f
Merrill Lines <linesmerrill@gmail.com>
2019-03-16 12:52:15 -0700

    echo

--- auto.bat
+++ auto.bat
@@ -1,9 +1,9 @@
 @ECHO OFF &setlocal
 for /l %%x in (1, 1, 100) do (
-    SET "random=%RANDOM%"
-    echo random >> config.txt
-    SET "commitMsg=random + '-new edits'"
+    SET random=%RANDOM%
+    echo %random% >> config.txt
+    SET commitMsg=%random% + "-new edits"
     call git add .
-    call git commit -m %%%commitMsg%%%
+    call git commit -m echo %commitMsg%
     call git push
 )
