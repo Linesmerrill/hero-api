b14a29d455dee8b30cd1b4dd6d43efa6943cb760
Merrill Lines <linesmerrill@gmail.com>
2019-03-16 12:55:51 -0700

    %

--- auto.bat
+++ auto.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 for /l %%x in (1, 1, 100) do (
-    SET random=%RANDOM% + 
+    SET random=%RANDOM% + %%x
     echo %random% >> config.txt
     SET commitMsg=%random% + "-new edits"
     call git add .
