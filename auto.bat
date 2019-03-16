10439a9820dd0e09d6c2ca56995a61ec3edd2faa
Merrill Lines <linesmerrill@gmail.com>
2019-03-16 13:03:38 -0700

    commitMsg

--- auto.bat
+++ auto.bat
@@ -1,9 +1,11 @@
 @ECHO OFF
 setlocal EnableDelayedExpansion
 for /l %%x in (1, 1, 100) do (
-    call SET random=!%RANDOM%%x%!
-    echo %random% >> config.txt
-    call SET commitMsg=%random% + "-new edits"
+    SET random=%RANDOM%
+    SET randomString=%x%
+    SET newvar=!%random%%randomString%!
+    echo %newvar% >> config.txt
+    SET commitMsg=!%newvar%!
     call git add .
     call git commit -m %%commitMsg
     call git push
