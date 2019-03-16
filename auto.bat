5bf26c862db49dba719c03cc0f509e7394001718
Merrill Lines <linesmerrill@gmail.com>
2019-03-16 13:05:51 -0700

    commitMsg

--- auto.bat
+++ auto.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 for /l %%x in (1, 1, 100) do (
     call SET random=%RANDOM%
-    echo %random%%% >> config.txt
+    echo %random% >> config.txt
     SET commitMsg=%%%random%%%
     call git add .
     call git commit -m %%commitMsg
